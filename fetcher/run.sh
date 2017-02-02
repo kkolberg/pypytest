5aa732e1db303e10dc6a848fa0d39db0f6b2966c
Korey Kolberg <koreyk@slalom.com>
2017-02-02 12:42:12 -0500

    files

---
+++ fetcher/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+CONFIG_S3=$1
+
+echo "----- starting virtualenv -----"
+sudo -H pip install virtualenv
+virtualenv .venv
+echo "----- finished virtualenv -----"
+
+echo "----- activating virtualenv and install pip-----"
+. .venv/bin/activate
+pip install -r requirements.txt
+echo "----- activated virtualenv and installed pip -----"
+
+echo "----- starting app.py with config $CONFIG_S3 -----"
+pypy app.py --config $CONFIG_S3
+echo "----- app.py finished -----"
+
--- splitter/app.py
+++ splitter/app.py
@@ -4,16 +4,22 @@ Chunk a very large JSON file into smaller 10,000 record files
 from time import time
 import ijson.backends.yajl2_cffi as ijson
 import simplejson as json
+import argparse
+import smart_open
 
+def fetchConfig(s3Config):
+    with smart_open.smart_open(s3Config) as fin:
+        configuration = json.loads(fin.read().decode("utf-8"))
+    return configuration
 
-def load_json(filename):
+def load_json(configuration):
     """
     Stream filename into new files in large chunks.
     """
     current_row = 0
     files = 1
     records = []
-    with open(filename, 'rt') as __fd:
+    with open(configuration["input"], 'rt') as __fd:
         objects = ijson.items(__fd, 'item')
         people = (o for o in objects)
         chunk_size = 15000
@@ -26,7 +32,7 @@ def load_json(filename):
                 end_time = time()
                 time_taken = end_time - start_time
                 print "file " + str(files) + " - " + str(time_taken)
-                write_output(records, files)
+                write_output(records, files, configuration)
                 start_time = time()
                 records = []
                 current_row = 0
@@ -36,20 +42,29 @@ def load_json(filename):
         end_time = time()
         time_taken = end_time - start_time
         print "file " + str(files) + " - " + str(time_taken)
-        write_output(records, files)
+        write_output(records, files, configuration)
 
-def write_output(records, files):
+def write_output(records, files, configuration):
     """
     Write output file to disk
     """
     __f = None
-    with open("./temp/sub-" + str(files) + ".json", "w+") as __f:
+    with open(configuration["output"]+"/sub-" + str(files) + ".json", "w+") as __f:
         for __p in records:
             __f.write(json.dumps(__p) + '\n')
 
 
 if __name__ == "__main__":
+    PARSER = argparse.ArgumentParser(description="Split massive file")
+    PARSER.add_argument(
+        "--config",
+        help="where in s3 to load configuration")
+
+    ARGS = PARSER.parse_args()
     GLOBAL_START_TIME = time()
-    load_json("./data.json")
+
+    configuration = fetchConfig(ARGS.config)
+    load_json(configuration)
+
     GLOBAL_END_TIME = time()
     print "total time - " + str(GLOBAL_END_TIME - GLOBAL_START_TIME)
